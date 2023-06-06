1f1d848c173f87651bce79ec34063038e1a19dc2
TxKien <81473046+txkien02@users.noreply.github.com>
2023-06-06 21:50:03 +0700

    update

--- xulianhso-model/image_process.py
+++ xulianhso-model/image_process.py
@@ -10,39 +10,37 @@ import util
 model_cfg_path = os.path.join('.', 'model', 'cfg', 'darknet-yolov3.cfg')
 model_weights_path = os.path.join('.', 'model', 'weights', 'model.weights')
 
-input_dir = sys.argv[1]
+input_dir = sys.argv[1]  # Lấy đường dẫn thư mục đầu vào từ tham số dòng lệnh
 
 for img_name in os.listdir(input_dir):
-    # Xóa danh sách độ chính xác
+    # Xóa danh sách độ chính xác và biển số xe trước mỗi ảnh
     accuracy_scores = []
     license_plate_list = []
 
-    img_path = os.path.join(input_dir, img_name)
+    img_path = os.path.join(input_dir, img_name)  # Đường dẫn đến ảnh
 
-
-    # Tải mô hình
+    # Tải mô hình YOLOv3
     net = cv2.dnn.readNetFromDarknet(model_cfg_path, model_weights_path)
 
     # Đọc ảnh
     img = cv2.imread(img_path)
-    H, W, _ = img.shape
+    H, W, _ = img.shape  # Kích thước ảnh (chiều cao, chiều rộng)
 
-    # Chuyển đổi ảnh
+    # Chuyển đổi ảnh thành dạng blob để đưa vào mạng neural
     blob = cv2.dnn.blobFromImage(img, 1 / 255, (416, 416), (0, 0, 0), True)
 
-    # Nhận dạng
-    net.setInput(blob)
+    # Nhận dạng đối tượng trong ảnh bằng cách đưa blob vào mạng neural
     detections = util.get_outputs(net)
 
-    # Lưu trữ thông tin bounding boxes, class IDs và scores
+    # Lưu trữ thông tin về bounding box, class ID và score của các đối tượng được nhận dạng
     bboxes = []
     class_ids = []
     scores = []
 
     for detection in detections:
-        bbox = detection[:4]  # [x1, x2, x3, x4]
+        bbox = detection[:4]  # [x1, y1, x2, y2]
         xc, yc, w, h = bbox
-        bbox = [int(xc * W), int(yc * H), int(w * W), int(h * H)]
+        bbox = [int(xc * W), int(yc * H), int(w * W), int(h * H)]  # Chuyển đổi tọa độ về không gian ảnh gốc
         bbox_confidence = detection[4]
         class_id = np.argmax(detection[5:])
         score = np.amax(detection[5:])
@@ -50,27 +48,26 @@ for img_name in os.listdir(input_dir):
         class_ids.append(class_id)
         scores.append(score)
 
-    # Áp dụng Non-Maximum Suppression (NMS)
+    # Áp dụng Non-Maximum Suppression (NMS) để loại bỏ các bounding box trùng lặp
     bboxes, class_ids, scores = util.NMS(bboxes, class_ids, scores)
 
-    
-    # Nhận dạng biển số xe và hiển thị thông tin
+    # Sử dụng EasyOCR để nhận dạng biển số xe và hiển thị thông tin
     reader = easyocr.Reader(['en'])
     for bbox_, bbox in enumerate(bboxes):
         xc, yc, w, h = bbox
-        license_plate = img[int(yc - (h / 2)):int(yc + (h / 2)), int(xc - (w / 2)):int(xc + (w / 2)), :].copy()
+        license_plate = img[int(yc - (h / 2)):int(yc + (h / 2)), int(xc - (w / 2)):int(xc + (w / 2)), :].copy()  # Cắt ảnh biển số
         img = cv2.rectangle(img, (int(xc - (w / 2)), int(yc - (h / 2))), (int(xc + (w / 2)), int(yc + (h / 2))),
-                            (0, 255, 0), 15)
+                            (0, 255, 0), 15)  # Vẽ bounding box lên ảnh gốc
         license_plate_gray = cv2.cvtColor(license_plate, cv2.COLOR_BGR2GRAY)
         _, license_plate_thresh = cv2.threshold(license_plate_gray, 64, 255, cv2.THRESH_BINARY_INV)
-        output = reader.readtext(license_plate_thresh)
+        output = reader.readtext(license_plate_thresh)  # Nhận dạng thông tin trên biển số
 
         for out in output:
             text_bbox, text, text_score = out
-            if text_score > 0.4:
+            if text_score > 0.4:  # Lọc kết quả có độ chính xác lớn hơn ngưỡng
                 print(text, text_score)
                 license_plate_list.append(str(text))
-                accuracy_scores.append(text_score)  # Thêm độ chính xác vào danh sách
+                accuracy_scores.append(text_score)  # Lưu trữ độ chính xác vào danh sách
 
     # Hiển thị biểu đồ cuối cùng với độ chính xác
     plt.figure()
@@ -78,9 +75,9 @@ for img_name in os.listdir(input_dir):
     plt.title('License Plate Recognition')
     plt.axis('off')
 
-    # Hiển thị thông tin độ chính xác lên biểu đồ
+    # Hiển thị thông tin độ chính xác và biển số lên biểu đồ
     for i, (score, plate) in enumerate(zip(accuracy_scores, license_plate_list)):
-        y = 200 + i * 100  # Thay đổi giá trị 70 để cách nhau nhiều hơn
+        y = 200 + i * 100  # Vị trí hiển thị trên trục y (có thể điều chỉnh)
         plt.text(10, y, f"Accuracy: {score:.2f} - Plate: {plate}", color='red', fontsize=10, fontweight='bold')
 
     plt.show()
--- xulianhso-model/util.py
+++ xulianhso-model/util.py
@@ -1,29 +1,31 @@
 import numpy as np
 import cv2
 
-
-def NMS(boxes, class_ids, confidences, overlapThresh = 0.5):
-
+# Hàm Non-Maximum Suppression (NMS) để loại bỏ các bounding box trùng lặp
+def NMS(boxes, class_ids, confidences, overlapThresh=0.5):
+    # Chuyển đổi thành mảng numpy
     boxes = np.asarray(boxes)
     class_ids = np.asarray(class_ids)
     confidences = np.asarray(confidences)
 
-    # Return empty lists, if no boxes given
+    # Trả về danh sách rỗng nếu không có bounding box nào
     if len(boxes) == 0:
         return [], [], []
 
-    x1 = boxes[:, 0] - (boxes[:, 2] / 2)  # x coordinate of the top-left corner
-    y1 = boxes[:, 1] - (boxes[:, 3] / 2)  # y coordinate of the top-left corner
-    x2 = boxes[:, 0] + (boxes[:, 2] / 2)  # x coordinate of the bottom-right corner
-    y2 = boxes[:, 1] + (boxes[:, 3] / 2)  # y coordinate of the bottom-right corner
+    # Tính toán tọa độ các góc của bounding box
+    x1 = boxes[:, 0] - (boxes[:, 2] / 2)
+    y1 = boxes[:, 1] - (boxes[:, 3] / 2)
+    x2 = boxes[:, 0] + (boxes[:, 2] / 2)
+    y2 = boxes[:, 1] + (boxes[:, 3] / 2)
 
+    # Tính toán diện tích của các bounding box
     areas = (x2 - x1 + 1) * (y2 - y1 + 1)
 
     indices = np.arange(len(x1))
     for i, box in enumerate(boxes):
-        # Create temporary indices
+        # Tạo các chỉ số tạm thời
         temp_indices = indices[indices != i]
-        # Find out the coordinates of the intersection box
+        # Tìm tọa độ của bounding box giao nhau
         xx1 = np.maximum(box[0] - (box[2] / 2), boxes[temp_indices, 0] - (boxes[temp_indices, 2] / 2))
         yy1 = np.maximum(box[1] - (box[3] / 2), boxes[temp_indices, 1] - (boxes[temp_indices, 3] / 2))
         xx2 = np.minimum(box[0] + (box[2] / 2), boxes[temp_indices, 0] + (boxes[temp_indices, 2] / 2))
@@ -32,35 +34,30 @@ def NMS(boxes, class_ids, confidences, overlapThresh = 0.5):
         w = np.maximum(0, xx2 - xx1 + 1)
         h = np.maximum(0, yy2 - yy1 + 1)
 
-        # compute the ratio of overlap
+        # Tính tỷ lệ giao nhau
         overlap = (w * h) / areas[temp_indices]
-        # if overlapping greater than our threshold, remove the bounding box
+        # Nếu tỷ lệ giao nhau lớn hơn ngưỡng, loại bỏ bounding box
         if np.any(overlap) > overlapThresh:
             indices = indices[indices != i]
 
-    # return only the boxes at the remaining indices
+    # Trả về chỉ các bounding box còn lại
     return boxes[indices], class_ids[indices], confidences[indices]
 
 
+# Hàm lấy kết quả từ mạng neural
 def get_outputs(net):
-
     layer_names = net.getLayerNames()
-
     output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
-
     outs = net.forward(output_layers)
-
     outs = [c for out in outs for c in out if c[4] > 0.1]
-
     return outs
 
 
+# Hàm vẽ bounding box lên ảnh
 def draw(bbox, img):
-
     xc, yc, w, h = bbox
     img = cv2.rectangle(img,
                         (xc - int(w / 2), yc - int(h / 2)),
                         (xc + int(w / 2), yc + int(h / 2)),
                         (0, 255, 0), 20)
-
     return img
